5b8cfbb2cb3da0da159f5c33b498f56448fe8b8e
Tushar Rathi <rathitushar2021@gmail.com>
2025-05-16 23:58:43 +0530

    Checking cors

--- server/src/app.js
+++ server/src/app.js
@@ -7,7 +7,7 @@ config();
 
 const app = express();
 
-app.use(cors({ origin:process.env.CLIENT_URL, credentials: true }));
+//app.use(cors({ origin:process.env.CLIENT_URL, credentials: true }));
 app.use(express.json());
 
 app.use(clerkMiddleware());
