daa2b7c3ec1e7ea528768691dba4880a871d1e53
Tushar Rathi <rathitushar2021@gmail.com>
2025-05-17 02:47:56 +0530

    Added cors

--- server/api/index.js
+++ server/api/index.js
@@ -13,7 +13,14 @@ const PORT = process.env.PORT || 5000;
 
 const app = express();
 
-//app.use(cors({ origin:process.env.CLIENT_URL, credentials: true }));
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 app.use(clerkMiddleware());
