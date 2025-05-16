7eaa2f30f9fd5f3c5a19b2ac4a1ab7f62924a1b4
Tushar Rathi <rathitushar2021@gmail.com>
2025-05-17 02:31:19 +0530

    Project restructuring

--- server/index.js
+++ server/index.js
@@ -1,10 +1,28 @@
 import app from "./src/app.js";
 import { connectToDatabase } from "./src/repo/connection.js";
+import express from "express";
+
+//import cors from "cors";
+import { config } from "dotenv";
+import { clerkMiddleware } from "@clerk/express";
+import appRouter from "./src/routes/routes.js";
+config();
 
 const PORT = process.env.PORT || 5000;
 
+const app = express();
+
+//app.use(cors({ origin:process.env.CLIENT_URL, credentials: true }));
+app.use(express.json());
+
+app.use(clerkMiddleware());
+
+app.use("/api", appRouter);
+
 connectToDatabase()
   .then(() => {
-    app.listen(PORT, () => console.log("Server connected to MongoDB, running on port 5000"));
+    app.listen(PORT, () =>
+      console.log("Server connected to MongoDB, running on port 5000")
+    );
   })
   .catch((err) => console.error(err));
